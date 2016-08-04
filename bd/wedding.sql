632340fe92e72841bb0ac7b3cc43feebb0f06df4
jenniferarce <arcej95@hotmail.com>
2016-08-04 18:09:22 -0300

    bd actualizado

--- bd/wedding.sql
+++ bd/wedding.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-02-2016 a las 20:44:51
+-- Tiempo de generación: 04-08-2016 a las 23:07:24
 -- Versión del servidor: 5.6.25
 -- Versión de PHP: 5.6.11
 
@@ -94,25 +94,9 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `provincia` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `direccion` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `localidad` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `foto` varchar(255) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'foto del usuario'
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `cliente`
---
-
-INSERT INTO `cliente` (`id`, `usuario`, `clave`, `nombre`, `telefono`, `email`, `provincia`, `direccion`, `localidad`, `foto`) VALUES
-(1, 'jenn', '1234', 'jja', 12345678, 'j@j.com', NULL, NULL, NULL, NULL),
-(2, 'lala', '12', 'lala', 123456, 'l@l.com', 'Buenos Aires', 'Alsina 600', 'Avellaneda', NULL),
-(3, 'p123', '345', 'pepe p', 74125896, 'pepe@pee.com', 'Buenos Aires', 'Andres Baranda 200', 'Quilmes', NULL),
-(4, 'aa', 'aa1234', 'ameba b', 4567321, 'am@laala.com', 'Buenos Aires', 'Juncal 2020', 'Capital Federal', NULL),
-(5, 'pepita12', 'lala12', 'pepita pink', 45612389, 'pep@p.com', 'Buenos Aires', 'Avenida Paseo colon 645', 'Capital Federal', NULL),
-(6, 'mia_lu', 'c632a6e0a5238669aa0a4cc8ce56944e', 'mia mlq', 34560765, 'mm@m.com', 'Buenos Aires', 'Avenida Santa Fe 3002', 'Capital Federal', NULL),
-(7, 'lilo', '81dc9bdb52d04dc20036dbd8313ed055', 'lilo y s', 78912356, 'lilo@l.com', 'null', '', '', NULL),
-(9, 'jj', '81dc9bdb52d04dc20036dbd8313ed055', 'JJ', 12345678, 'killingsicname@gmail.com', 'Buenos Aires', 'Avenida Bartolome Mitre 3800', 'Sarandi', NULL),
-(16, 'abbe', '202cb962ac59075b964b07152d234b70', 'a', 45629631, 'abb@c.com', 'null', '', '', NULL),
-(18, 'ab', '202cb962ac59075b964b07152d234b70', 'a', 12345678, 'a@ab.com', 'null', '', '', '559443_236111653212191_712934173_n.jpg'),
-(19, 'abby', '202cb962ac59075b964b07152d234b70', 'Abby B', 12345896, 'abb@abbe.com', 'Buenos Aires', 'Avenida Hipolito Yrigoyen 1700', 'Avellaneda', 'fotos/abby.jpg');
+  `foto` varchar(255) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'foto del usuario',
+  `tipo_usuario` varchar(10) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'cliente o admin'
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -127,20 +111,6 @@ CREATE TABLE IF NOT EXISTS `invitado` (
   `pariente` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `nromesa` varchar(5) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `invitado`
---
-
-INSERT INTO `invitado` (`user`, `dni`, `nomyape`, `pariente`, `nromesa`) VALUES
-('abby', 40950053, 'Rosita', 'hermano-a', 'm2'),
-('jj', 30000151, 'leia', 'cuniado-a', 'm2'),
-('mia_lu', 1000005, 'hh', 'hermano-a', 'm2'),
-('mia_lu', 1000027, 'ui', 'suegro-a', 'm2'),
-('mia_lu', 1000054, 'pepi', 'tio-a', 'm3'),
-('mia_lu', 1000083, 'Pepe Pepo', 'tio-a', 'm2'),
-('mia_lu', 10058960, 'ty', 'cuniado-a', 'm2'),
-('mia_lu', 30580087, 'cf', 'primo-a', 'm2');
 
 --
 -- Índices para tablas volcadas
@@ -167,7 +137,7 @@ ALTER TABLE `invitado`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
